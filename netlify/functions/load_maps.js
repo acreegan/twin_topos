9542ee26a2a9bb696bfb52ca485c53fcf5d38ab3
andrew <andrew.s.creegan@gmail.com>
2023-05-21 18:46:14 +1200

    .

--- netlify/functions/load_maps.js
+++ netlify/functions/load_maps.js
@@ -6,12 +6,7 @@ exports.handler = async (event, context) => {
 
   const url = `https://maps.googleapis.com${event.path}&key=${apiKey}`; // Append the API key to the URL
   const response = await fetch(url);
-  body = response.body
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" , response: body}),
-    };
+  
 
   if (!response.ok) {
     return {
