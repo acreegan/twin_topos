71cb9e399b743b3481ad504273adb906766a583e
andrew <andrew.s.creegan@gmail.com>
2023-05-21 17:57:00 +1200

    adding netlfy function to proxy request to google api

---
+++ netlify/functions/load_maps.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+const apiKey = process.env.MAPS_API_KEY;
+
+exports.handler = async (event, context) => {
+  const url = `https://maps.googleapis.com${event.path}&key=${apiKey}`; // Append the API key to the URL
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    return {
+      statusCode: response.status,
+      body: response.statusText
+    };
+  }
+
+  const data = await response.text();
+
+  return {
+    statusCode: 200,
+    body: data
+  };
+};
