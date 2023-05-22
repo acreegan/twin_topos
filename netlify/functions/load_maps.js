2b552342c021029c2bd67ddfa4acdf12e5c7540a
andrew <andrew.s.creegan@gmail.com>
2023-05-22 18:24:51 +1200

    setup to test locally

--- netlify/functions/load_maps.js
+++ netlify/functions/load_maps.js
@@ -1,20 +1,13 @@
 const fetch = require('node-fetch');
+require('dotenv').config();
 const apiKey = process.env.MAPS_API_KEY;
 
 exports.handler = async (event, context) => {
 
-
   const url = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`; // Append the API key to the URL
   const response = await fetch(url);
   body = response.body
-
-
-
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({ message: "Hello World" , code:response.status, response: body}),
-//     };
-
+  
   if (!response.ok) {
     return {
       statusCode: response.status,
